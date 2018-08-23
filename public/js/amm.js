f0df659488d23cffec88bdc96540bf8f70ff0942
nellika <nynelli@yahoo.de>
2018-08-23 16:24:47 +0200

    refactor

--- public/js/amm.js
+++ public/js/amm.js
@@ -1,141 +1,97 @@
-var passwords = {setStep1:"bd", setStep2:"Mario", setStep3:"lifeIsRunning", setStep4:"littlePrince", setStep5:"pizza", setStep6:"fincsi",setStep7:"GOT",setStep8:"boldog"}
+var steps = [
+  {step: 0,
+  title: 'Szép jó reggelt Ammur (26)!',
+  instruction: '<p>Úgy látszik, megértél egy kis kalandra...</p>' +
+               '<p>Készen állsz?</p>',
+  btnTxt: "I'm in!",
+  img: {src: 'img/hi-50.jpg', alt:"Link says hi to your birthday"},
+  pwd: 'null'},
 
-function setStep0(){
-  toggleInput('hide');
-  $('.title').text('Szép jó reggelt Ammur (26)!');
-  $('.long-text').html('<p>Úgy látszik, megértél egy kis kalandra...</p>' +
-                       '<p>Készen állsz?</p>');
-  $('.next-btn').text("I'm in!");
-  $('.prev-btn').text("Nah...");
-  $('.text-img img').attr("src","img/hi-50.jpg");
-  $('.text-img img').attr("alt","Link says hi to your birthday");
+  {step: 1,
+  title: 'Éhes vándor nem jut messzire...',
+  instruction: '<p>Indulnál már, tudom,</p>' +
+               '<p>De mi lesz így az úton?</p><br>' +
+               '<p>Szükség van energiára</p>' +
+               '<p>Vadászat ennek az ára!</p><br>' +
+               '<p><span class="location">Konyhá</span>ban az eszköz, s a vad</p>' +
+               '<p>A <span class="location">szekrény</span>ből el nem szalad.</p>',
+  btnTxt: 'Jelentem, ettem!',
+  img: {src: 'img/hungry-50.jpg', alt:"Hungry adventurer"},
+  pwd: 'null'},
 
-}
+  {step: 2,
+  title: 'No de a felszerelés?',
+  instruction: '<p>Mielőtt kilépnél a házból,</p>' +
+               '<p>A <span class="location">kamrá</span>t nézd meg jól!</p><br>' +
+               '<p>Találsz ott foltos zsákot</p>' +
+               '<p>Rakhatod bele kis kardod.</p>',
+  btnTxt: 'Megvan!',
+  img: {src: 'img/bpack-50.jpg', alt:"Adventurer with backpack"},
+  pwd: 'Mario'},
 
-function setStep1(){
-  changeText('Éhes vándor nem jut messzire...');
-  /*$('.title').text('Éhes vándor nem jut messzire...');*/
-  changeHTML('<p>Indulnál már, tudom,</p>' +
-                       '<p>De mi lesz így az úton?</p><br>' +
-                       '<p>Szükség van energiára</p>' +
-                       '<p>Vadászat ennek az ára!</p><br>' +
-                       '<p>Konyhában az eszköz, s a vad</p>' +
-                       '<p>A szekrényből el nem szalad.</p>'
-                     );
-  $('.next-btn').text("Jelentem, ettem!");
-  $('.prev-btn').text("Nah...");
-  $('.text-img img').attr("src","img/hungry-50.jpg");
-  $('.text-img img').attr("alt","Hungry adventurer");
+  {step: 3,
+  title: 'Nehéz a munka...',
+  instruction: '<p>Neki vághatsz most már!</p>' +
+               '<p>A vonat ugyan nem vár...</p><br>' +
+               '<p><span class="location">Munkahely</span>ed üresnek tűnhet.</p>' +
+               '<p>Kis szekrényed azért nézd meg!</p><br>' +
+               '<p>Az <span class="location">alsó fiók</span>ban tán?</p>' +
+               '<p>Ajándék nem terem a fán...</p>',
+  btnTxt: 'Indul a munka!',
+  img: {src: 'img/work-50.jpg', alt:"The hero starts working"},
+  pwd: 'lifeIsRunning'},
 
-}
+  {step: 4,
+  title: 'Harc a munkában...',
+  instruction: '<p>Dolgozni sokat kell...</p>' +
+               '<p>Délután 5-ig a harcos csak gépel.</p><br>' +
+               '<p>Amint vége az izgalmaknak,</p>' +
+               '<p>Adj egy esélyt egy kis <span class="location">japán</span>nak.</p><br>' +
+               '<p>Van egy hely, hol sok az étel,</p>' +
+               '<p>SZÉP kártyánknak ez nem tétel.</p>' +
+               '<p>5-kor keresd vacsorádat</p>' +
+               '<p>Addig végezd csak a munkádat.</p>',
+  btnTxt: 'Megvacsiztam.',
+  img: {src: 'img/fight-50.jpg', alt:"The hero is fighting"},
+  pwd: 'littlePrince'},
 
-function setStep2(){
-  changeText('No de a felszerelés?');
-  /*$('.title').text('No de a felszerelés?');*/
-  changeHTML('<p>Mielőtt kilépnél a házból,</p>' +
-                       '<p>A kamrát nézd meg jól!</p><br>' +
-                       '<p>Találsz ott foltos zsákot</p>' +
-                       '<p>Rakhatod bele kis kardod.</p><br>'
-                     );
-  $('.next-btn').text("Megvan!");
-  $('.prev-btn').text("Nah...");
-  $('.text-img img').attr("src","img/bpack-50.jpg");
-  $('.text-img img').attr("alt","Adventurer with backpack");
+  {step: 5,
+  title: 'A harcos el nem fárad!',
+  instruction: '<p>A vacsora kissé felpörgetett,</p>' +
+               '<p>Ideje megnézni a desszertet.</p><br>' +
+               '<p>Kérd hűséges <span class="location">társ</span>ad segítségét.</p>' +
+               '<p>Mutassa meg a kis sütikéjét.</p>',
+  btnTxt: 'Desszerthas',
+  img: {src: 'img/zelda-50.jpg', alt:"Loyal buddy"},
+  pwd: 'pizza'},
 
-}
+  {step: 6,
+  title: 'Indulás haza!',
+  instruction: '<p>Bár jó volt kint,</p>' +
+               '<p>Nézzük meg, otthon, hogy, s mint</p><br>' +
+               '<p>Lehet, hogy a <span class="location">TV mellett</span></p>' +
+               '<p>Meg kéne nézni a <span class="location">szekrény</span>eket?</p>',
+  btnTxt: 'Wááá!',
+  img: {src: 'img/dance-50.jpg', alt:"Dancing buddy"},
+  pwd: 'fincsi'},
 
-function setStep3(){
-  changeText('Nehéz a munka...');
-  /*$('.title').text('Nehéz a munka...');*/
-  changeHTML('<p>Neki vághatsz most már!</p>' +
-                       '<p>A vonat ugyan nem vár...</p><br>' +
-                       '<p>Munkahelyed üresnek tűnhet.</p>' +
-                       '<p>Kis szekrényed azért nézd meg!</p><br>' +
-                       '<p>Az alsó fiókban tán?</p>' +
-                       '<p>Ajándék nem terem a fán...</p>'
-                     );
-  $('.next-btn').text("Indul a munka!");
-  $('.prev-btn').text("Nah...");
-  $('.text-img img').attr("src","img/work-50.jpg");
-  $('.text-img img').attr("alt","The hero starts working");
+  {step: 7,
+  title: 'Végső próba',
+  instruction: '<p>Hosszú nap, sok fáradalom,</p>' +
+               '<p>Ideje már aludnom.</p><br>' +
+               '<p>Vagy lehet meg kéne tekintenem,</p>' +
+               '<p>Mit rejtettek az ingembe?</p>',
+  btnTxt: 'Vége?',
+  img: {src: 'img/sleep-50.jpg', alt:"Sleepy?"},
+  pwd: 'GOT'},
 
-}
-
-function setStep4(){
-  changeText('Harc a munkában...');
-  /*$('.title').text('Harc a munkában...');*/
-  changeHTML('<p>Dolgozni sokat kell...</p>' +
-                       '<p>Délután 5-ig a harcos csak gépel.</p><br>' +
-                       '<p>Amint vége az izgalmaknak,</p>' +
-                       '<p>Adj egy esélyt egy kis japánnak.</p><br>' +
-                       '<p>Van egy hely, hol sok az étel,</p>' +
-                       '<p>SZÉP kártyánknak ez nem tétel.</p>' +
-                       '<p>5-kor keresd vacsorádat</p>' +
-                       '<p>Addig végezd csak a munkádat.</p>'
-                     );
-  $('.next-btn').text("Megvacsiztam.");
-  $('.prev-btn').text("Nah...");
-  $('.text-img img').attr("src","img/fight-50.jpg");
-  $('.text-img img').attr("alt","The hero is fighting");
-
-}
-
-function setStep5(){
-  changeText('A harcos el nem fárad!');
-  /*$('.title').text('A harcos el nem fárad!');*/
-  changeHTML('<p>A vacsora kissé felpörgetett,</p>' +
-                       '<p>Ideje megnézni a desszertet.</p><br>' +
-                       '<p>Kérd hűséges társad segítségét.</p>' +
-                       '<p>Mutassa meg a kis sütikéjét.</p><br>'
-                     );
-  $('.next-btn').text("Desszerthas.");
-  $('.prev-btn').text("Nah...");
-  $('.text-img img').attr("src","img/zelda-50.jpg");
-  $('.text-img img').attr("alt","Loyal buddy");
-
-}
-
-function setStep6(){
-  changeText('Indulás haza!');
-  /*$('.title').text('Indulás haza!');*/
-  changeHTML('<p>Bár jó volt kint,</p>' +
-                       '<p>Nézzük meg, otthon, hogy, s mint</p><br>' +
-                       '<p>Lehet, hogy a TV mellett</p>' +
-                       '<p>Meg kéne nézni a szekrényeket?</p><br>'
-                     );
-  $('.next-btn').text("Wááá!");
-  $('.prev-btn').text("Nah...");
-  $('.text-img img').attr("src","img/dance-50.jpg");
-  $('.text-img img').attr("alt","Dancing buddy");
-
-}
-
-function setStep7(){
-  changeText('Végső próba');
-  /*$('.title').text('Végső próba');*/
-  changeHTML('<p>Hosszú nap, sok fáradalom,</p>' +
-                       '<p>Ideje már aludnom.</p><br>' +
-                       '<p>Vagy lehet meg kéne tekintenem,</p>' +
-                       '<p>Mit rejtettek az ingembe?</p><br>'
-                     );
-  $('.next-btn').text("Vége?");
-  $('.prev-btn').text("Nah...");
-  $('.text-img img').attr("src","img/sleep-50.jpg");
-  $('.text-img img').attr("alt","Sleepy?");
-
-}
-
-function setStep8(){
-  changeText('Itt a vége...!');
-  /*$('.title').text('Itt a vége...!');*/
-  changeHTML('<p>Boldog születésnapot kalandorom, Ammurom!</p>');
-  $('.input-pwd').css('display','none');
-  $('.actions').css('display','none');
-  $('.text-img img').css('max-width','100%');
-  $('.text-img img').attr("src","img/happy-50.jpg");
-  $('.text-img img').attr("alt","Happy adventurer");
-
-}
+  {step: 8,
+  title: 'Itt a vége...!',
+  instruction: '<p>Boldog születésnapot kalandorom, Ammurom!</p>',
+  btnTxt: '',
+  img: {src: 'img/happy-50.jpg', alt:"Happy adventurer"},
+  pwd: 'boldog'}
+];
 
 function toggleInput(action){
   var inputField = $('.input-pwd');
@@ -148,18 +104,18 @@ function toggleInput(action){
 
 function nextStep(){
   var next = + $('.step-holder').text() +1;
-  var getNextStep = "setStep" + next;
-  var inputValue = $('.input-pwd #pwd').val();
+  var nextStep = steps[next];
+  var pwdNeeded = steps[next+1]?steps[next+1].pwd:'null';
 
-  if(passwords[getNextStep] == inputValue){
-    scrollTop();
-    $('.step-holder').text(next);
-    var fn = window[getNextStep];
-    toggleInput('show');
-    fn();
-    $('.input-pwd #pwd').val('');
+  if(nextStep.pwd == 'null'){
+    setStep(nextStep,pwdNeeded);
   }else{
-    $(".invalid-pwd").removeClass('initial no-show').addClass("show");
+    var inputValue = $('.input-pwd #pwd').val();
+    if(nextStep.pwd == inputValue){
+      setStep(nextStep,pwdNeeded);
+    }else{
+      $(".invalid-pwd").removeClass('initial no-show').addClass("show");
+    }
   }
 
 }
@@ -173,8 +129,7 @@ function negativeStep(){
                        '<p>Ha segítség kell, hívj! ^^</p>' +
                        '<p></p>');
   $('.back-btn').text("Folytatom!");
-  $('.text-img img').attr("src","img/wrong2-50.jpg");
-  $('.text-img img').attr("alt","Not a good direction");
+  changeImage("img/wrong2-50.jpg","Not a good direction");
 }
 
 function setTwoButtons(){
@@ -192,12 +147,10 @@ function setTwoButtons(){
 function setOneButton(){
   $('.actions').html('<button class="back-btn" type="button"></button>');
   $( ".back-btn" ).click(function() {
-      var currentStep = $('.step-holder').text();
-      var resetStep = 'setStep' + currentStep;
-      var fn = window[resetStep];
-      toggleInput('show');
+      var currentStep = $('.step-holder').text()*1;
+      var nextPwd = steps[currentStep+1]?steps[currentStep+1].pwd:'null';
       setTwoButtons();
-      fn();
+      setStep(steps[currentStep],nextPwd);
   });
 }
 
@@ -207,7 +160,7 @@ function addClickToAlert(){
   });
 }
 
-function stupInputField(){
+function setupInputField(){
   var input = $("#pwd");
 
   input.keyup(function(event) {
@@ -225,36 +178,15 @@ function scrollTop(){
 }
 
 $(function() {
-    console.log( "Page is ready! ^^" );
-    var ruha = $('.sec-holder').attr('kabat');
-
-    if(ruha == "teli"){
-      secu();
-      $( "#guardian" ).click(function() {
-        var inputValue = $('#initpwd').val();
-        if(inputValue == evszak){
-          letsDoIt();
-        }
-      });
-    }else{
-      letsDoIt();
-    }
-});
-
-
-
-function secu(){
-  $('body').append('<div class="guards"><img id="guardian" src="img/guard-50.jpg" alt="Guardian of the secret page">'+
-                   '<div><input type="text" name="initpwd" id="initpwd" placeholder="Insert secret code"></div>' +
-                   '<p>Btw. Aki kíváncsi, hamar megöregszik, Ammurom ;)</p></div>');
-}
+  letsDoIt();
+})
 
 function letsDoIt(){
   $('.wrapper').css('display','block');
   $('.guards').css('display','none');
-  stupInputField();
+  setupInputField();
   setTwoButtons();
-  setStep0();
+  nextStep();
   addClickToAlert();
 }
 
@@ -270,4 +202,33 @@ function changeHTML(html){
   });
 }
 
-var evszak = 'kacsa123';
+function changeBtnText(nextTxt){
+  $('.next-btn').text(nextTxt);
+  $('.prev-btn').text("Nah...");
+}
+
+function changeImage(src, alt){
+  $('.text-img img').attr("src",src);
+  $('.text-img img').attr("alt",alt);
+}
+
+function setStep(stepObj,pwdNeeded){
+  scrollTop();
+  changeText(stepObj.title);
+  changeHTML(stepObj.instruction);
+  changeBtnText(stepObj.btnTxt);
+  if(stepObj.step == steps.length-1){
+    $('.actions').css('display','none');
+    $('.text-img img').css('max-width','100%');
+  }
+  changeImage(stepObj.img.src,stepObj.img.alt);
+  $('.input-pwd #pwd').val('');
+
+  if(pwdNeeded == 'null'){
+    toggleInput('hide');
+  }else{
+    toggleInput('show');
+  }
+
+  $('.step-holder').text(stepObj.step);
+}
